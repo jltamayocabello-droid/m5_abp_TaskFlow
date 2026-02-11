a52495bcacce991659e95a2747b8cdb9a505fd8c
Jorge <jltamayocabello@gmail.com>
2026-02-11 17:45:32 -0300

    Se añade referencias al DOM y la interactividad (Eventos) a main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
-
-
 import { GestorTareas } from "./classes/GestorTareas.js";
 
+// GESTOR DE TAREAS
 const gestor = new GestorTareas();
-gestor.agregarTarea("Aprender JavaScript", "Repasar módulos y clases ES6");
-gestor.agregarTarea("Comprar café", "Necesario para programar");
-console.log("Estado actual del gestor:", gestor.tareas);
 
+//==========================================
+// REFERENCIAS AL DOM
+//==========================================
 
+//Capturamos los elementos del HTML usando los IDs
+const formulario = document.querySelector("#form-tarea");
+const inputTitulo = document.querySelector("#input-titulo");
+const inputDescripcion = document.querySelector("#input-descripcion");
+const listaTareas = document.querySelector("#lista-tareas");
+
+//PRUEBA DE CONEXION
+console.log("Elementos DOM capturados:", {
+  formulario,
+  inputTitulo,
+  listaTareas,
+});
+
+//==========================================
+// EVENTOS (La interactividad)
+//==========================================
+
+// Capturamos el evento submit del formulario
+formulario.addEventListener("submit", (event) => {
+  // Detenemos la recarga automática de la web
+  event.preventDefault();
+
+  // Validamos que haya texto en el input
+  const titulo = inputTitulo.value.trim();
+  const descripcion = inputDescripcion.value.trim();
+
+  if (titulo === "") return; //Si está vacío no hace nada
+
+  // Crear tarea usando GESTOR
+  const nuevaTarea = gestor.agregarTarea(titulo, descripcion);
+
+  // Depuración (Verificamos en consola que se creó)
+  console.log("Tarea creada:", nuevaTarea);
+  console.log("Lista actual:", gestor.tareas);
+
+  // Limpiamos el formulario
+  formulario.reset();
+});
